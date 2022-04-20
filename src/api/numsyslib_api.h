4b9c62a80c8f39d2fe7ebace59ecb3dcfc9930a1
ivankprod <houseprotector@mail.ru>
2022-04-20 19:07:25 +0500

    refactor(api): format srouce

--- src/api/numsyslib_api.h
+++ src/api/numsyslib_api.h
@@ -1,12 +1,7 @@
+// Numeric System Library API
 
-/*
-  	Numeric System™ Library API
-  	
-  	© 2008-2016 IvanK Production
-*/
-
-#ifndef _NUMSYSLIB_API_H_
-#define _NUMSYSLIB_API_H_
+#ifndef NUMSYSLIB_API_H
+#define NUMSYSLIB_API_H
 
 #ifdef BUILDING_DLL
 #define NSL_API __declspec(dllexport)
@@ -16,22 +11,22 @@
 
 #include <windows.h>
 
-extern "C" {
-	NSL_API void   nslAboutLib(HWND hWndOwner = 0);
-	NSL_API char * nslGetLibVersion();
+extern "C"
+{
+	NSL_API void nslAboutLib(HWND hWndOwner = 0);
+	NSL_API char *nslGetLibVersion();
 
-	NSL_API int    nslGetLastError();
-	NSL_API bool   nslResetErrors();
-	NSL_API char * nslConvert(char * lpsValue, int iInNS, int iOutNS);
-	NSL_API int    nslSetPrecision(char * lpsPrecision, int iOldPrec);
+	NSL_API int nslGetLastError();
+	NSL_API bool nslResetErrors();
+	NSL_API char *nslConvert(char *lpsValue, int iInNS, int iOutNS);
+	NSL_API int nslSetPrecision(char *lpsPrecision, int iOldPrec);
 
-	NSL_API bool   nslMemClear(int iIndex);
-	NSL_API char * nslMemRead(int iIndex);
-	NSL_API bool   nslMemAdd(int iIndex, char * lpsValue);
-	NSL_API bool   nslMemSub(int iIndex, char * lpsValue);
-	NSL_API bool   nslMemMul(int iIndex, char * lpsValue);
-	NSL_API bool   nslMemDiv(int iIndex, char * lpsValue);
+	NSL_API bool nslMemClear(int iIndex);
+	NSL_API char *nslMemRead(int iIndex);
+	NSL_API bool nslMemAdd(int iIndex, char *lpsValue);
+	NSL_API bool nslMemSub(int iIndex, char *lpsValue);
+	NSL_API bool nslMemMul(int iIndex, char *lpsValue);
+	NSL_API bool nslMemDiv(int iIndex, char *lpsValue);
 }
 
 #endif
-
